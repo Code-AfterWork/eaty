a8b2ac8b886440725170d5e8b893c6507647cad4
Moses Mbadi <mosesmbadi@gmail.com>
2023-05-11 22:07:18 +0300

    added food to /food_category endpoint

--- mealgenerator/serializers.py
+++ mealgenerator/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import FoodCategory, Food
+from .models import *
 
 class FoodSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,8 +7,27 @@ class FoodSerializer(serializers.ModelSerializer):
         fields = ['name']
 
 class FoodCategorySerializer(serializers.ModelSerializer):
-    foods = FoodSerializer(many=True, read_only=True)
+    # foods = FoodSerializer(many=True, read_only=True)
+    food = serializers.StringRelatedField(many=True, source='food_set')
 
     class Meta:
         model = FoodCategory
-        fields = ['name', 'foods']
+        fields = ['name', 'food']
+
+
+# Alternatively, if you want more control over the serialization
+# of the Food objects, you can create a separate serializer for
+# the Food model and use it within the FoodCategorySerializer:
+
+# class FoodSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Food
+#         fields = ['name']
+
+# class FoodCategorySerializer(serializers.ModelSerializer):
+#     foods = FoodSerializer(many=True)
+
+#     class Meta:
+#         model = FoodCategory
+#         fields = ['name', 'foods']
+
