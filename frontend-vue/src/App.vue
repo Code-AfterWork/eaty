de506f2a52bf43f11ccf4147b5f2fde65d258911
Moses Mbadi <mosesmbadi@gmail.com>
2023-05-27 20:17:01 +0300

    updated readme

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -4,21 +4,6 @@ import HelloWorld from './components/meals.vue'
 import Login from './components/Login.vue'
 </script>
 
-<!-- <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/meals">Meals</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template> -->
 
 <template>
   <header>
